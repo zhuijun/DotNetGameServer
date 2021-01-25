99ecef648d67634a8771953e6fd09825119f1bfc
zhanghj <zhanghj@qq.com>
2021-01-25 12:27:30 +0800

    MessageHandleImpl

--- DBServer/DBServer/Game/MessageHandleImpl.cs
+++ DBServer/DBServer/Game/MessageHandleImpl.cs
@@ -5,14 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+#nullable enable
 namespace DBServer.Game
 {
     public class MessageHandleImpl : IMessageHandle
     {
+        private readonly Func<ForwardMailMessage, Func<MailboxMessage, Task>, Task>? _Handles;
+
+        public MessageHandleImpl()
+        {
+            _Handles += OnEnterRole;
+        }
+
+
         public async Task HandleMessage(ForwardMailMessage forwardMail, Func<MailboxMessage, Task> replyMailAction)
         {
+            if (_Handles != null)
+            {
+                await _Handles.Invoke(forwardMail, replyMailAction);
+            }
+        }
 
-            await replyMailAction(new MailboxMessage { Id = forwardMail.Id, Content = forwardMail.Content });
+        private async Task OnEnterRole(ForwardMailMessage forwardMail, Func<MailboxMessage, Task> replyMailAction)
+        {
+            if (forwardMail.Id == 1)
+            {
+                await replyMailAction(new MailboxMessage { Id = forwardMail.Id, Content = forwardMail.Content });
+            }
         }
     }
 }
