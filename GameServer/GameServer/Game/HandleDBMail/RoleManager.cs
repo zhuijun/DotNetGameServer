32d8e3584858d369124e57b44cc5f6534bdd92d2
zhanghj <zhanghj@qq.com>
2021-02-20 09:10:30 +0800

    ReplayResult

--- GameServer/GameServer/Game/HandleDBMail/RoleManager.cs
+++ GameServer/GameServer/Game/HandleDBMail/RoleManager.cs
@@ -16,17 +16,17 @@ namespace GameServer.Game
             switch (mail.Id)
             {
                 case (int)GameDBProto.MessageID.EnterRoleReplyId:
-                    OnEnterRoleReply(mail);
+                    DBEnterRoleReply(mail);
                     break;
                 default:
                     break;
             }
         }
 
-        private void OnEnterRoleReply(MailPacket mail)
+        private void DBEnterRoleReply(MailPacket mail)
         {
             var replay = GameDBProto.EnterRoleReply.Parser.ParseFrom(mail.Content);
-            var stoc = new ClientServerProto.StoCEnterRoleReply {Result =  new ClientServerProto.StoCReplayResult { ErrorCode = replay.Result.ErrorCode, ErrorInfo = replay.Result.ErrorInfo }, RoleID = replay.RoleID };
+            var stoc = new ClientServerProto.StoCEnterRoleReply {Result =  new ClientServerProto.ReplayResult { ErrorCode = replay.Result.ErrorCode, ErrorInfo = replay.Result.ErrorInfo }, RoleID = replay.RoleID };
             Dispatcher.WriteAgentMail(new MailPacket { Id = (int)ClientServerProto.MessageID.StoCenterRoleReplyId, Content = stoc.ToByteArray(), Reserve = mail.Reserve, UserId = mail.UserId, ClientId = mail.ClientId });
         }
     }
