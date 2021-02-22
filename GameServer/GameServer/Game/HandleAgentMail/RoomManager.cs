a8c28d6b4a9890d012232b23b2d946972bfa5012
zhanghj <zhanghj@qq.com>
2021-02-22 16:20:49 +0800

    OnJoinDeskRequest

--- GameServer/GameServer/Game/HandleAgentMail/RoomManager.cs
+++ GameServer/GameServer/Game/HandleAgentMail/RoomManager.cs
@@ -18,6 +18,12 @@ namespace GameServer.Game
                 case (int)ClientServerProto.MessageId.CtoScreateDeskRequestId:
                     OnCreateDeskRequest(mail);
                     break;
+                case (int)ClientServerProto.MessageId.CtoSjoinDeskRequestId:
+                    OnJoinDeskRequest(mail);
+                    break;
+                case (int)ClientServerProto.MessageId.CtoSleaveDeskRequestId:
+                    OnLeaveDeskRequest(mail);
+                    break;
                 default:
                     var roleId = ManagerMediator.RoleManager.GetRoleIdByClientId(mail.ClientId);
                     var desk = _room.GetDesk(_room.GetRoleDesk(roleId));
@@ -74,6 +80,7 @@ namespace GameServer.Game
                 var game = _gameFactory.CreateGame(1);
                 var desk = _room.CreateDesk(game, 3);
                 deskId = desk.DeskId;
+                desk.AddRole(roleId);
                 _room.AddRoleDesk(roleId, deskId);
 
                 stoc.DeskId = deskId;
@@ -87,6 +94,75 @@ namespace GameServer.Game
             Dispatcher.WriteAgentMail(new MailPacket
             {
                 Id = (int)ClientServerProto.MessageId.StoCcreateDeskReplyId,
+                Content = stoc.ToByteArray(),
+                Reserve = mail.Reserve,
+                UserId = mail.UserId,
+                ClientId = mail.ClientId
+            });
+        }
+
+        private void OnJoinDeskRequest(MailPacket mail)
+        {
+            var stoc = new ClientServerProto.StoCJoinDeskReply();
+
+            var roleId = ManagerMediator.RoleManager.GetRoleIdByClientId(mail.ClientId);
+            var ctos = ClientServerProto.CtoSJoinDeskRequest.Parser.ParseFrom(mail.Content);
+            var deskId = ctos.DeskId;
+            var desk = _room.GetDesk(deskId);
+            if (desk != null)
+            {
+                desk.AddRole(roleId);
+                _room.AddRoleDesk(roleId, deskId);
+            }
+            else
+            {
+                stoc.Result.ErrorCode = 1;
+                stoc.Result.ErrorInfo = "not find desk";
+            }
+
+            Dispatcher.WriteAgentMail(new MailPacket
+            {
+                Id = (int)ClientServerProto.MessageId.StoCjoinDeskReplyId,
+                Content = stoc.ToByteArray(),
+                Reserve = mail.Reserve,
+                UserId = mail.UserId,
+                ClientId = mail.ClientId
+            });
+        }
+
+        private void OnLeaveDeskRequest(MailPacket mail)
+        {
+            var stoc = new ClientServerProto.StoCLeaveDeskReply();
+
+            var roleId = ManagerMediator.RoleManager.GetRoleIdByClientId(mail.ClientId);
+            var deskId = _room.GetRoleDesk(roleId);
+            if (deskId != 0)
+            {
+                var desk = _room.GetDesk(deskId);
+                if (desk != null)
+                {
+                    if (desk.GameLogic is IAgentMail agentMail)
+                    {
+                        var request = new LeaveGameRequest { UserId = mail.UserId };
+                        agentMail.OnLeaveGame(request, mail.ClientId);
+                    }
+                    desk.RemoveRole(roleId);
+                    if (desk.RoleCount() == 0)
+                    {
+                        _room.RemoveDesk(desk.DeskId);
+                    }
+                    _room.RemoveRoleDesk(roleId);
+                }
+            }
+            else
+            {
+                stoc.Result.ErrorCode = 1;
+                stoc.Result.ErrorInfo = "not on desk";
+            }
+
+            Dispatcher.WriteAgentMail(new MailPacket
+            {
+                Id = (int)ClientServerProto.MessageId.StoCleaveDeskReplyId,
                 Content = stoc.ToByteArray(),
                 Reserve = mail.Reserve,
                 UserId = mail.UserId,
